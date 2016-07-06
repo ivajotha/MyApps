65ab825825d0a87cf407e6dc9f9ac0846671ad1a
ivajotha <jonathan@MacBook-Pro-de-Ivan.local>
2016-07-06 14:49:04 -0500

    Add Toolbar

---
+++ app/src/main/java/mx/ivajotha/myapps/AddActivity.java
@@ -0,0 +1,16 @@
+package mx.ivajotha.myapps;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jonathan on 06/07/16.
+ */
+public class AddActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
--- app/src/main/java/mx/ivajotha/myapps/MainActivity.java
+++ app/src/main/java/mx/ivajotha/myapps/MainActivity.java
@@ -1,13 +1,41 @@
 package mx.ivajotha.myapps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_APP_ADD = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Add Toolbar*/
+        Toolbar toolbar = (Toolbar)findViewById(R.id.mytoolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_main_add:
+                startActivityForResult(new Intent(getApplicationContext(), AddActivity.class),
+                        REQUEST_APP_ADD);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
